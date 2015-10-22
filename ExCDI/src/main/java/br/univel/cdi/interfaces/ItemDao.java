d914e2889496d5b22781d18323a190cced885105
Matheus Apel <matheusz.apel@gmail.com>
2015-10-22 21:59:59 -0200

    Criados os arquivos web.xml e process.xhtml

--- ExCDI/src/main/java/br/univel/cdi/interfaces/ItemDao.java
+++ ExCDI/src/main/java/br/univel/cdi/interfaces/ItemDao.java
@@ -2,10 +2,13 @@ package br.univel.cdi.interfaces;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import br.univel.cdi.entidades.Item;
 
 public interface ItemDao {
 
+	@Inject
 	List<Item> fetchItems();
 	
 }
