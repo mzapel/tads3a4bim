caca752d85122468d865fbff2de54bba936ec620
Matheus Apel <matheusz.apel@gmail.com>
2015-10-22 21:48:23 -0200

    Criada a classe DefaultItemDao no pacote br.univel.cdi.dao

---
+++ ExCDI/src/main/java/br/univel/cdi/dao/DefaultItemDao.java
@@ -0,0 +1,21 @@
+package br.univel.cdi.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.univel.cdi.entidades.Item;
+import br.univel.cdi.interfaces.ItemDao;
+
+public class DefaultItemDao implements ItemDao {
+
+	@Override
+	public List<Item> fetchItems() {
+		List<Item> results = new ArrayList<Item>();
+		results.add(new Item(34, 7));
+		results.add(new Item(4, 37));
+		results.add(new Item(24, 19));
+		results.add(new Item(89, 32));
+		return results;
+	}
+
+}
